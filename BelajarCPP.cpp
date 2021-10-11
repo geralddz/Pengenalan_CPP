ae2f678b55480b621607d94ee178aecee4e67491
GeraldDZ <80369682+geralddz@users.noreply.github.com>
2021-10-11 08:37:13 +0700

    Add files via upload

---
+++ BelajarCPP.cpp
@@ -0,0 +1,24 @@
+//Belajar C++
+
+#include <iostream> //Header
+#include <conio.h>
+
+using namespace std;
+
+ main(){
+
+	system ("color A");
+	cout<<"================================================================\n";
+	cout<<"|\tPerkenalkan nama saya \t\t\t\t\t|\n";
+	cout<<"|\tGerald Dzulfiqar Adhitama\t\t\t\t|\n";
+	cout<<"|\tSaya Asisten Praktikum\t\t\t\t\t|\n";
+	cout<<"|\tAlgoritma Struktur Data\t\t\t\t\t|\n";
+	cout<<"|\tD3MI01\t\t\t\t\t\t\t|\n";
+	cout<<"|\tDengan Dosen Pengampu\t\t\t\t\t|\n";
+	cout<<"|\tBu Yuli Astuti M.Kom\t\t\t\t\t|\n";
+	cout <<"|\tSELAMAT BELAJAR C++\t\t\t\t\t|\n";
+	cout << "================================================================\n" ;
+	
+	getch();
+	return 0;
+}
